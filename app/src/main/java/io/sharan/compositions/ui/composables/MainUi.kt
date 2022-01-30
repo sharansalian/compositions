650fbc58718ca8a54cb9ed670da030072877a9ac
Sharan Salian <sharansalian.dev@gmail.com>
2022-01-30 12:42:32 +0530

    Add initial MainUi

---
+++ app/src/main/java/io/sharan/compositions/ui/composables/MainUi.kt
@@ -0,0 +1,17 @@
+package io.sharan.compositions.ui.composables
+
+import androidx.compose.runtime.Composable
+
+
+/**
+ * Created by sharan on 30/01/22.
+ */
+
+/**
+ * Its the entry point for the jetpack compose UI.
+ * It accepts the main view model and subscribes to the StateFlow for the new UI State
+ * We keep it follow MVI architecture smoothly
+ */
+@Composable
+fun MainUi() {
+}
