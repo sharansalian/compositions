8a73e3b8dcebb4431b2c97ed3498cd8436a67a66
Sharan Salian <sharansalian.dev@gmail.com>
2022-01-30 12:37:49 +0530

    Add App

---
+++ app/src/main/java/io/sharan/compositions/framework/App.kt
@@ -0,0 +1,11 @@
+package io.sharan.compositions.framework
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by sharan on 30/01/22.
+ */
+@HiltAndroidApp
+class App : Application()
