2b229af8c9e2ec06e2038cb3d8981217b5e4e292
Sharan Salian <sharansalian.dev@gmail.com>
2022-01-30 12:44:17 +0530

    Add PersonCard.kt

---
+++ app/src/main/java/io/sharan/compositions/ui/composables/PersonCard.kt
@@ -0,0 +1,18 @@
+package io.sharan.compositions.ui.composables
+
+import android.app.Person
+import androidx.compose.runtime.Composable
+
+
+/**
+ * Created by sharan on 30/01/22.
+ */
+
+
+/**
+ * Composable to create a person card 1 box
+ */
+@Composable
+fun PersonCard(person: Person) {
+
+}
